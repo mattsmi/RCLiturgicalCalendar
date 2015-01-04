0251301aee5c67363017651e7fd43da1f8664a59
mattsmi <mtgs@hotmail.com>
2015-01-04 13:16:13 +1100

    adjusted for new bootstrap

--- RCcal/PrintRCCal.php
+++ RCcal/PrintRCCal.php
@@ -33,11 +33,16 @@ function pPrintRCCalendar($iYear, $iMonth, $sLang, $iEDM, $sCalendarChosen) {
 	echo ('<link rel="stylesheet" href="../generic/css/monthcal.css">' . "\n");
 	
 	echo ('<!-- Basic Twitter Bootstrap stylesheet -->' . "\n");
-	echo ('<link rel="stylesheet" href="../generic/css/bootstrap.css">' . "\n\n");
+	echo ('<link rel="stylesheet" href="../generic/css/bootstrap.min.css">' . "\n\n");
 	echo ('<!-- Theme stylesheet -->' . "\n");
 	echo ('<link rel="stylesheet" href="../generic/css/bootstrap-theme.css">' . "\n\n");
-	echo ('<!-- Bootstrap JavaScript -->' . "\n");
-	echo ('<script src="../generic/js/bootstrap.js"></script>' . "\n\n");
+	
+	echo ('<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->' . "\n");
+	echo ('<!--[if lt IE 9]>' . "\n");
+	echo ('<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>' . "\n");
+	echo ('<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>' . "\n");
+    echo ('<![endif]-->' . "\n");
+	
 	echo ('<!-- Proper rendering and touch zooming -->' . "\n");
 	echo ('<meta name="viewport" content="width=device-width, initial-scale=1">' . "\n");
 	
@@ -284,7 +289,18 @@ function pPrintRCCalendar($iYear, $iMonth, $sLang, $iEDM, $sCalendarChosen) {
 	echo ("</form>\n");
 	
 	// End of the HTML page
-	echo ("</div></body>\n</html>\n");
+	echo ("</div>\n");
+	
+	echo ('<!-- Bootstrap core JavaScript' . "\n");
+	echo ('================================================== -->' . "\n");
+	echo ('<!-- Placed at the end of the document so the pages load faster -->' . "\n");
+	echo ('<script src="../generic/js/jquery.min.js"></script>' . "\n");
+	echo ('<script src="../generic/js/bootstrap.min.js"></script>' . "\n");
+	echo ('		<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->' . "\n");
+	echo ('		<script src="../generic/js/ie10-viewport-bug-workaround.js"></script>' . "\n");
+	
+	
+	echo("</body>\n</html>\n");
 }
 function pFindOptionalMemoriae($sOptMem) {
 	
